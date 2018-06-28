ace3b61a0aa22425a1fdb14f1d30bac427751d0a
luftkluft <luftkluft@gmail.com>
2018-06-28 23:58:34 +0300

    mailer3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               { host: 'https://todo-luftkluft.herokuapp.com/' },
+  domain:               "gmail.com",
   user_name:            Rails.application.secrets.user_name,
   password:             Rails.application.secrets.user_password,
   authentication:       'plain',
