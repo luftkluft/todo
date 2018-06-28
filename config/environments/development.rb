91ea0081fc58e35684d360c5057bd7e926679453
luftkluft <luftkluft@gmail.com>
2018-06-28 23:41:51 +0300

    mailer1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,15 +37,15 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  # address:              'smtp.gmail.com',
-  # port:                 587,
-  # domain:               'example.com',
-  # user_name:            '<username>',
-  # password:             '<password>',
-  # authentication:       'plain',
-  # enable_starttls_auto: true }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'localhost:3000',
+  user_name:            Rails.application.secrets.user_name,
+  password:             Rails.application.secrets.user_password,
+  authentication:       'plain',
+  enable_starttls_auto: true }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'https://todo-luftkluft.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'localhost:3000',
+  user_name:            Rails.application.secrets.user_name,
+  password:             Rails.application.secrets.user_password,
+  authentication:       'plain',
+  enable_starttls_auto: true }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
