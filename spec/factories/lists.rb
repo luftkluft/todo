a4e2b19e21f4ff16a25ee5ed7625f14e3b7bc82f
luftkluft <luftkluft@gmail.com>
2020-10-18 13:49:14 +0300

    Rspec/dev: create factories

--- spec/factories/lists.rb
+++ spec/factories/lists.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
   factory :list do
+    title_list    { Faker::Lorem.sentence(word_count: 5) }
+    priority_list { 1 }
+    deadline_list { "2018-06-19 01:43:25" }
+    status        { "new" }
   end
 end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :task do
+    association :list, factory: :list
+    content_task  { Faker::Lorem.sentence(word_count: 10) }
+    priority_task { 1 }
+    deadline_task { "2018-06-19 01:47:57" }
+    status        { :in_work }
   end
 end
