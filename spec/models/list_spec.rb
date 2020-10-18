8d08e118173c730362e51ffae832e65c537aa89e
luftkluft <luftkluft@gmail.com>
2020-10-18 14:07:24 +0300

    Rspec/dev: user, task, list models

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-
+  it { should validate_presence_of :title_list }
+  it { should have_many(:tasks) }
+  it { should belong_to(:user) }
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-
+  it { should validate_presence_of :content_task }
+  it { should belong_to(:list) }
 end
