41c96a2dae0fce7c59bf306c11dc5eac5060d7b5
luftkluft <luftkluft@gmail.com>
2020-10-20 19:11:21 +0300

    HotFix/master: make default sorting by priority

--- app/assets/javascripts/landing.js
+++ app/assets/javascripts/landing.js
@@ -1,9 +1,22 @@
 $(document).ready(function () {
+  var str1 = $('#nav_status').text();
+  str1 = str1.split(' ').join('');
+  var str2 = $('#nav_lists').text();
+  str2 = str2.split(' ').join('');
+  var str3 = $('#nav_tasks').text();
+  str3 = str3.split(' ').join('');
+
   $.ajax({
-    type: "GET",
-    url: '/lists',
+    cache: false,
+    url: "/sorting",
+    type: 'GET',
+    data: 'status=' + str1 + ';lists=' + str2 + ';tasks=' + str3,
+    dataType: 'html',
+
     success: function (data) {
-      $(".lists").html(data)
+      $(".lists").html(data);
     }
-  })
+
+  });
+  return false;
 });
